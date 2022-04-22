115fb8352c26541f6469b36a878f68ec458553b4
Zarckash <zarckash@gmail.com>
2022-04-22 17:14:01 +0200

    Fix Redundancy

--- Scripts/ZKTool.ps1
+++ Scripts/ZKTool.ps1
@@ -629,25 +629,12 @@ $PaddingPanel.Width              = 695
 $PaddingPanel.Location           = New-Object System.Drawing.Point(($PanelSize*0),594)
 $Form.Controls.Add($PaddingPanel)
 
-$Buttons = @($SB1,$SB2,$SB3,$SB4,$SB5,$SB6,$SB7,$SB8,$SB9,$SB10,$SB11,$MoreS,$MSB1,$MSB2,$MSB3,$MSB4,$MSB5,$MSB6,$MSB7,$MSB8,$MSB9,$MSB10,$MSB11,$MSB12,
-$LB1,$LB2,$LB3,$LB4,$LB5,$LB6,$LB7,$LB8,$TB1,$TB2,$TB3,$TB4,$TB5,$TB6,$TB7,$TB8,$TB9,$TB10,$MoreT,$MTB1,$MTB2,$MTB3,$MTB4,$MTB5,$MTB6,
-$MTB7,$MTB8,$MTB9,$MTB10,$MTB11,$HSB1,$HSB2,$HSB3,$HB1,$HB2,$HB3,$HB4,$HB5,$HB6,$HB7,$HB8,$HB9,$HTB1,$HTB2,$HTB3)
-foreach ($Button in $Buttons) {
-    $Button.Width                = 215
-    $Button.Height               = 35
-    $Button.Font                 = New-Object System.Drawing.Font('Ubuntu Mono',12)
-    $Button.BackColor            = $ButtonColor
-}
-
-$TB1.Height                      = 72
-$MTB1.Height                     = 72
-
 
 $Buttons = @($SB1,$SB2,$SB3,$SB4,$SB5,$SB6,$SB7,$SB8,$SB9,$SB10,$SB11,$MSB1,$MSB2,$MSB3,$MSB4,$MSB5,$MSB6,$MSB7,$MSB8,$MSB9,$MSB10,$MSB11,$MSB12,
 $LB1,$LB2,$LB3,$LB4,$LB5,$LB6,$LB7,$LB8,$TB1,$TB2,$TB3,$TB4,$TB5,$TB6,$TB7,$TB8,$TB9,$TB10,$MTB1,$MTB2,$MTB3,$MTB4,$MTB5,$MTB6,
 $MTB7,$MTB8,$MTB9,$MTB10,$MTB11,$HSB1,$HSB2,$HSB3,$HB1,$HB2,$HB3,$HB4,$HB5,$HB6,$HB7,$HB8,$HB9,$HTB1,$HTB2,$HTB3)
-foreach ($Button in $Buttons) {
 
+foreach ($Button in $Buttons) {
     $FormTextColor = [System.Drawing.ColorTranslator]::FromHtml("#F1F1F1")
     $SelectedTextColor = [System.Drawing.ColorTranslator]::FromHtml("#000000")
     $TextColor = [System.Drawing.ColorTranslator]::FromHtml("#99FFFD")
@@ -676,6 +663,17 @@ foreach ($Button in $Buttons) {
         }
     })
 }
+
+$Buttons += @($MoreS,$MoreT)
+foreach ($Button in $Buttons) {
+    $Button.Width                = 215
+    $Button.Height               = 35
+    $Button.Font                 = New-Object System.Drawing.Font('Ubuntu Mono',12)
+    $Button.BackColor            = $ButtonColor
+}
+
+$TB1.Height                      = 72
+$MTB1.Height                     = 72
 
 $MoreS.Add_MouseEnter({
     if ($this.BackColor -eq $ButtonColor) {
