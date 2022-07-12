c29197080c6d3177947a1f3e4a0782adb69157e4
Zarckash <zarckash@gmail.com>
2022-07-12 03:06:18 +0200

    Improve User Output

--- Scripts/Initialize.ps1
+++ Scripts/Initialize.ps1
@@ -12,31 +12,37 @@ if (!(Test-Path -Path "$env:windir\System32\ZKTool.exe")) {
     Invoke-WebRequest -Uri "https://github.com/Zarckash/ZKTool/raw/main/Apps/ZKTool.exe" -OutFile "$env:windir\System32\ZKTool.exe"
     Invoke-WebRequest -Uri "https://github.com/Zarckash/ZKTool/raw/main/Apps/ZKTool.lnk" -OutFile "C:\Users\Public\Desktop\ZKTool.lnk"
     New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
-    New-Item -Path "HKCR:\Directory\Background\shell\" -Name "ZKTool"
-    New-Item -Path "HKCR:\Directory\Background\shell\ZKTool\" -Name "command"
+    New-Item -Path "HKCR:\Directory\Background\shell\" -Name "ZKTool" | Out-Null
+    New-Item -Path "HKCR:\Directory\Background\shell\ZKTool\" -Name "command" | Out-Null
     Set-ItemProperty -Path "HKCR:\Directory\Background\shell\ZKTool\" -Name "Icon" -Value "ZKTool.exe,0"
     Set-ItemProperty -Path "HKCR:\Directory\Background\shell\ZKTool\command\" -Name "(default)" -Value "ZKTool.exe"
+    Write-Host "    Creando ZKTool.exe..."
 }
 
 # Check Winget
-Write-Host "Comprobando Winget..."
+Write-Host "`r`nComprobando Winget..."
 if (!(Test-Path ~\AppData\Local\Microsoft\WindowsApps\winget.exe)) {
-    Write-Host "Instalando Winget..."
+    Write-Host "    Instalando Winget..."
     Start-Process "ms-appinstaller:?source=https://aka.ms/getwinget"
 	$nid = (Get-Process AppInstaller).Id
 	Wait-Process -Id $nid
 }  
 
 # Check GUI Fonts
-Write-Host "Comprobando Fuentes Necesarias..."
+Write-Host "`r`nComprobando Fuentes Necesarias..."
 if (!(Test-Path -Path $env:userprofile\AppData\Local\Microsoft\Windows\Fonts\UbuntuMono*)) {
-    Write-Host "Instalando Fuentes Necesarias..."
+    Write-Host "    Instalando Fuentes Necesarias..."
 	Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Zarckash/ZKTool/main/Configs/FontUbuntuMono.zip" -OutFile $env:userprofile\AppData\Local\Temp\FontUbuntuMono.zip
     Expand-Archive $env:userprofile\AppData\Local\Temp\FontUbuntuMono.zip $env:userprofile\AppData\Local\Temp\FontUbuntuMono -Force
     Start-Process $env:userprofile\AppData\Local\Temp\FontUbuntuMono\Install.exe
     Start-Sleep 10
 }
 
-Write-Host "Ready"
+Write-Host "`r`n        ###################" -ForegroundColor Green
+Write-Host "        #####  READY  #####" -ForegroundColor Green
+Write-Host "        ###################" -ForegroundColor Green
 Start-Process $env:windir\System32\ZKTool.exe
+
+Start-Sleep 2
+
 Exit
