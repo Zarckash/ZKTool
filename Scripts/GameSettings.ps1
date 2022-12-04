1f9eca8660323c228111979a680429b82ba31c1d
Zarckash <zarckash@gmail.com>
2022-12-04 04:40:46 +0100

    Add More Settings

--- Scripts/GameSettings.ps1
+++ Scripts/GameSettings.ps1
@@ -157,7 +157,9 @@ $Download = New-Object net.webclient
 # Modern Warfare II
 $R1B1.Add_Click({
     $Download.DownloadFile($FromPath+"/Configs/ModernWarfareII.zip", $ToPath+"\Configs\ModernWarfareII.zip")
-    Expand-Archive -Path ($ToPath+"\Configs\ModernWarfareII.zip") -DestinationPath ($DocumentsPath+"\Call of Duty\players") -Force
+    Expand-Archive -Path ($ToPath+"\Configs\ModernWarfareII.zip") -DestinationPath ($ToPath+"\Configs\ModernWarfareII") -Force
+    Move-Item -Path ($ToPath+"\Configs\ModernWarfareII\options.3.cod22.cst") -Destination ($DocumentsPath+"\Call of Duty\players")
+    Move-Item -Path ($ToPath+"\Configs\ModernWarfareII\settings.3.local.cod22.cst") -Destination ($DocumentsPath+"\Call of Duty\players\" + (Get-ChildItem -Directory -Name 765*))
     $StatusBox.Text = "|Configuracion De " + $this.Text + " Aplicada...`r`n" + $StatusBox.Text
 })
 
