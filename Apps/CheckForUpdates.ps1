5a64ded1da592533aed34cbb24fc572fb02c8e91
Zarckash <zarckash@gmail.com>
2022-12-19 09:18:14 +0100

    No Longer Needed

--- Apps/CheckForUpdates.ps1
+++
@@ -1,5 +0,0 @@
-Invoke-WebRequest -Uri "https://github.com/Zarckash/ZKTool/raw/main/Apps/ZKTool.exe" -OutFile "$env:ProgramFiles\ZKTool\ZKTool.exe"
-Invoke-WebRequest -Uri "https://github.com/Zarckash/ZKTool/raw/main/Apps/ZKTool.lnk" -OutFile "$env:userprofile\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\ZKTool.lnk"
-Start-Process $env:ProgramFiles\ZKTool\ZKTool.exe
-Start-Sleep 2
-Exit
