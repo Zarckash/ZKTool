36fc109b3b211be886060e007cd0d34aae79828c
Zarckash <zarckash@gmail.com>
2023-08-03 22:52:42 +0200

    File Encoding To UTF-8 With BOM

--- Scripts/Initialize.ps1
+++ Scripts/Initialize.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = 'SilentlyContinue'
+﻿$ErrorActionPreference = 'SilentlyContinue'
 
 # Run Script As Administrator
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
