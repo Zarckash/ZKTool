cefd3a67e2f78f87971e5d21c837299a6a137aa5
Zarckash <zarckash@gmail.com>
2023-08-06 02:52:41 +0200

    UTF-8

--- Scripts/Initialize.ps1
+++ Scripts/Initialize.ps1
@@ -1,4 +1,4 @@
-﻿$ErrorActionPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'SilentlyContinue'
 
 # Run Script As Administrator
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
