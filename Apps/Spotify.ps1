a517744c28bf186e1b946e4bf23ba3c983a1dc11
Zarckash <zarckash@gmail.com>
2023-08-08 21:20:19 +0200

    Update Spotify.ps1

--- Apps/Spotify.ps1
+++ Apps/Spotify.ps1
@@ -1,6 +1,6 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 iex "& { $((iwr -useb 'https://raw.githubusercontent.com/amd64fox/SpotX/main/Install.ps1').Content) } -new_theme -confirm_uninstall_ms_spoti -confirm_spoti_recomended_over -podcasts_off -block_update_on"
-Add-Content $env:userprofile\AppData\Roaming\Spotify\prefs "app.autostart-configured=true`nui.hardware_acceleration=false`napp.autostart-mode=`"off`""
+Add-Content $env:appdata\Spotify\prefs "app.autostart-configured=true`nui.hardware_acceleration=false`napp.autostart-mode=`"off`""
 New-Item -Path "HKCU:\Software" -Name "Spotify"
 New-Item -Path "HKCU:\Software\Spotify" -Name "Window Position"
 Set-ItemProperty -Path "HKCU:\Software\Spotify\Window Position" -Name "Heigh" -Type DWord -Value 634
@@ -8,3 +8,4 @@ Set-ItemProperty -Path "HKCU:\Software\Spotify\Window Position" -Name "Left" -Ty
 Set-ItemProperty -Path "HKCU:\Software\Spotify\Window Position" -Name "Show State" -Type DWord -Value 1
 Set-ItemProperty -Path "HKCU:\Software\Spotify\Window Position" -Name "Top" -Type DWord -Value 203
 Set-ItemProperty -Path "HKCU:\Software\Spotify\Window Position" -Name "Width" -Type DWord -Value 1003
+Start-Sleep 1
