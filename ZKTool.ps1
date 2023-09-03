cb7a27bf6588e2de4c272f6be965b82fcf76fd11
Zarckash <zarckash@gmail.com>
2023-09-03 20:47:56 +0200

    Fix Images

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -46,12 +46,6 @@ $PanelBackColor        = [System.Drawing.ColorTranslator]::FromHtml("#2B2B2B") #
 $AccentColor           = [System.Drawing.ColorTranslator]::FromHtml("#ACA5F3") # Purple
 $DefaultForeColor      = [System.Drawing.ColorTranslator]::FromHtml("#FFFFFF") # White
 
-# Images Variables
-$ActiveButtonColor     = [System.Drawing.Image]::FromFile("$ImagesFolder\ActiveButtonColor.png")
-$HoverButtonColor      = [System.Drawing.Image]::FromFile("$ImagesFolder\HoverButtonColor.png")
-$ActiveButtonColorBig  = [System.Drawing.Image]::FromFile("$ImagesFolder\ActiveButtonColorBig.png")
-$HoverButtonColorBig   = [System.Drawing.Image]::FromFile("$ImagesFolder\HoverButtonColorBig.png")
-
 # Defining Lists
 $AppsList    = $Download.DownloadString("$GitHubPath/Resources/Apps.json")  | ConvertFrom-Json
 $FormsList   = $Download.DownloadString("$GitHubPath/Resources/Forms.json") | ConvertFrom-Json
@@ -474,10 +468,16 @@ $HB8.Location  = "458,42"
 $HB9.Text      = ""
 $HB9.Location  = "458,78"
 
-$Buttons = @($HB1,$HB2,$HB3,$HB4,$HB5,$HB6,$HB7,$HB8,$HB9)
+$Buttons = @($HB1,$HB2,$HB3,$HB4,$HB5,$HB6,$HB7)
 foreach ($Button in $Buttons) {
     $HPanel.Controls.Add($Button)
 }
+
+# Images Variables
+$ActiveButtonColor     = [System.Drawing.Image]::FromFile("$ImagesFolder\ActiveButtonColor.png")
+$HoverButtonColor      = [System.Drawing.Image]::FromFile("$ImagesFolder\HoverButtonColor.png")
+$ActiveButtonColorBig  = [System.Drawing.Image]::FromFile("$ImagesFolder\ActiveButtonColorBig.png")
+$HoverButtonColorBig   = [System.Drawing.Image]::FromFile("$ImagesFolder\HoverButtonColorBig.png")
 
 $Buttons = @($SB1,$SB2,$SB3,$SB4,$SB5,$SB6,$SB7,$SB8,$SB9,$SB10,$SB11,$SB12,$MSB1,$MSB2,$MSB3,$MSB4,$MSB5,$MSB6,$MSB7,$MSB8,$MSB9,$MSB10,$MSB11,$MSB12,$MSB13,$MSB14,$MSB15,$MSB16,
 $MSB17,$LB1,$LB2,$LB3,$LB4,$LB5,$LB6,$LB7,$LB8,$TB2,$TB3,$TB4,$TB5,$TB6,$TB7,$TB8,$TB9,$TB10,$TB11,$MTB2,$MTB3,$MTB4,$MTB5,$MTB6,$MTB7,$MTB8,$MTB9,$MTB10,$MTB11,$MTB12,
