f33e79133a2e77c57d9ac8def2b7131bcb95ff93
Zarckash <zarckash@gmail.com>
2023-09-19 09:23:40 +0200

    Create DisableDefender.ps1

---
+++ Files/DisableDefender.ps1
@@ -0,0 +1,87 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+$Host.UI.RawUI.WindowTitle = 'Windows Defender Disabler'
+
+function Write-TypeHost ([string]$s = '',[string]$TextColor = 'DarkCyan') {
+    $s -split '' | ForEach-Object {
+        Write-Host $_ -NoNewline -ForegroundColor $TextColor
+        Start-Sleep -Milliseconds 15
+    }
+    Start-Sleep -Milliseconds 15
+    Write-Host `n
+}
+
+$TempPath = "$env:temp\ZKTool\Files"
+
+#Disable Real Time Protection using virtual keyboard
+Write-TypeHost "Desactivando Proteccion En Tiempo Real..."
+
+Start-Process "explorer" -ArgumentList "windowsdefender://threat" 
+Start-Sleep 2
+
+$wshell = New-Object -ComObject wscript.shell
+
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Keyboard
+{
+    [DllImport("user32.dll")]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+}
+"@
+
+$wshell.SendKeys("{TAB}")
+Start-Sleep .65
+$wshell.SendKeys("{TAB}")
+Start-Sleep .65
+$wshell.SendKeys("{TAB}")
+Start-Sleep .65
+$wshell.SendKeys("{TAB}")
+Start-Sleep .65
+$wshell.SendKeys(" ")
+Start-Sleep .65
+$wshell.SendKeys(" ")
+
+Start-Sleep .75
+
+Stop-Process -Name "SecHealthUI"
+
+# Downloading files
+$Url = "https://github.com/Zarckash/ZKTool/raw/main/Files/.exe/NSudoLG.exe"
+(New-Object System.Net.WebClient).DownloadFile($Url,"$TempPath\NSudoLG.exe")
+
+# Create .bat file
+'@echo off
+
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\WdNisSvc" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\WinDefend" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\WdNisDrv" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\WdFilter" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\WdBoot" /v "Start" /t REG_DWORD /d "4" /f
+Reg.exe add "HKLM\SYSTEM\CurrentControlSet\Services\Sense" /v "Start" /t REG_DWORD /d "4" /f' | Out-File "$TempPath\DisableDefender.bat"
+
+#disables defender through gp edit
+Write-TypeHost "Desactivando Windows Defender Con Política De Grupo..." 
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1 -Force
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name "EnableSmartScreen" -Type DWord -Value 0 -Force
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "Real-Time Protection"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableRealtimeMonitoring" -Type DWord -Value 1 -Force
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\SecurityHealthService" -Name "Start" -Type DWord -Value 4 -Force
+
+Get-ScheduledTask -TaskName "Windows Defender Cache Maintenance" | Disable-ScheduledTask
+Get-ScheduledTask -TaskName "Windows Defender Cleanup" | Disable-ScheduledTask 
+Get-ScheduledTask -TaskName "Windows Defender Scheduled Scan" | Disable-ScheduledTask
+Get-ScheduledTask -TaskName "Windows Defender Verification" | Disable-ScheduledTask
+
+gpupdate /force
+
+# Running .bat file with Power Run
+Write-TypeHost "Desactivando Servicios..."
+$BatPath = "$TempPath\DisableDefender.bat"
+$ArgumentList = "-U:T -P:E -M:S " + "`"$BatPath`"" 
+Start-Process "$TempPath\NSudoLG.exe" -ArgumentList $ArgumentList -Wait
+
+
+Write-TypeHost '- - - WINDOWS DEFENDER DESACTIVADO - - -'
+Start-Sleep 2
