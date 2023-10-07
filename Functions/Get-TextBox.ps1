6351befeb836ae57c7ccfa743d5fbd2410ff226f
Zarckash <zarckash@gmail.com>
2023-10-07 08:38:54 +0200

    Select IP  and DNS functions

---
+++ Functions/Get-TextBox.ps1
@@ -0,0 +1,27 @@
+﻿function Get-TextBox {
+    $App.IPValues = @("IPBoxValue1","IPBoxValue2","IPBoxValue3","IPBoxValue4")
+    $App.DNS1Values = @("DNSBox1Value1","DNSBox1Value2","DNSBox1Value3","DNSBox1Value4")
+    $App.DNS2Values = @("DNSBox2Value1","DNSBox2Value2","DNSBox2Value3","DNSBox2Value4")
+
+
+    $App.IPValues | ForEach-Object {
+        if ($App.$_.Text -like "[0-9]*") {
+            $App.CustomIP += $App.$_.Text + "."
+        }
+    }
+    $App.CustomIP = $App.CustomIP.Substring(0,$App.CustomIP.Length - 1)
+
+    $App.DNS1Values | ForEach-Object {
+        if ($App.$_.Text -like "[0-9]*") {
+            $App.CustomDNS1 += $App.$_.Text + "."
+        }
+    }
+    $App.CustomDNS1 = $App.CustomDNS1.Substring(0,$App.CustomDNS1.Length - 1)
+
+    $App.DNS2Values | ForEach-Object {
+        if ($App.$_.Text -like "[0-9]*") {
+            $App.CustomDNS2 += $App.$_.Text + "."
+        }
+    }
+    $App.CustomDNS2 = $App.CustomDNS2.Substring(0,$App.CustomDNS2.Length - 1)
+}
--- Functions/Set-NetConfig.ps1
+++ Functions/Set-NetConfig.ps1
@@ -1,3 +1,79 @@
 ﻿function Set-NetConfig {
+    $Script:IP = $null
+    $Script:DNS1 = $null
+    $Script:DNS2 = $null
+
+    $Script:Interface = "Ethernet"
+    $Script:Interfaces = Get-NetIPConfiguration | Select-Object -ExpandProperty InterfaceAlias
+    if (($Interfaces.GetType()).IsArray) {
+    $Interfaces | ForEach-Object {Disable-NetAdapter $_ -Confirm:$false}
+    Enable-NetAdapter "Ethernet" -Confirm:$false
+    }
+
+    $Script:Gateway = Get-NetIPConfiguration | Select-Object -ExpandProperty IPv4DefaultGateway | Select-Object -ExpandProperty NextHop
+
+    if (!($null -eq $App.SelectedIP)) {
+        $Index = $App.SelectedIP -replace 'IP*', ''
+        $IP = $App.FoundIPList[$Index - 1]
+    }
     
+    if (!($null -eq $App.SelectedDNS)) {
+        switch ($App.SelectedDNS) {
+            DNS1 {
+                $DNS1 = "8.8.8.8"
+                $DNS2 = "8.8.4.4"
+            }
+            DNS2 {
+                $DNS1 = "1.1.1.1"
+                $DNS2 = "1.1.1.1"
+            }
+            DNS3 {
+                $DNS1 = $Gateway
+                $DNS2 = $Gateway
+            }
+            Default {}
+        }
+    }
+
+    if (!($null -eq $App.CustomIP)) {
+        $IP = $App.CustomIP
+    }
+
+    if (!($null -eq $App.CustomDNS1)) {
+        $DNS1 = $App.CustomDNS1
+        $DNS2 = $App.CustomDNS2
+    }
+
+
+
+    if (!($null -eq $IP)) {
+        Write-UserOutput "Estableciendo IP $IP"
+        Remove-NetIPAddress -InterfaceAlias $Interface -Confirm:$false
+        Remove-NetRoute -InterfaceAlias $Interface
+        New-NetIPAddress -InterfaceAlias $Interface -AddressFamily IPv4 $IP -PrefixLength 24 -DefaultGateway $Gateway | Out-Null
+    }
+
+    if (!($null -eq $DNS1)) {
+        Write-UserOutput "Estableciendo DNS $DNS1 y $DNS2"
+        Set-DnsClientServerAddress -InterfaceAlias $Interface -ServerAddresses $DNS1, $DNS2
+    }
+
+    Set-NetConnectionProfile -NetworkCategory Private
+    Disable-NetAdapter -Name $Interface -Confirm:$false
+    Enable-NetAdapter -Name $Interface -Confirm:$false
+
+    $App.SelectedIP = $null
+    $App.SelectedDNS = $null
+    $App.CustomIP = $null
+    $App.CustomDNS1 = $null
+    $App.CustomDNS2 = $null
+    $App.IPValues | ForEach-Object {
+        Update-GUI $_ Text $null
+    }
+    $App.DNS1Values | ForEach-Object {
+        Update-GUI $_ Text $null
+    }
+    $App.DNS2Values | ForEach-Object {
+        Update-GUI $_ Text $null
+    }
 }
