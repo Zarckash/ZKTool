424690c4e4afeae18b6c47c2d7cd91aba56e6d6f
Zarckash <zarckash@gmail.com>
2023-10-18 01:36:43 +0200

    Update to 4.0.9

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -5,7 +5,7 @@ $ProgressPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $ConfirmPreference = 'None'
 
-$App.Version = "4.0.8"
+$App.Version = "4.0.9"
 try {
     Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZKTool" -Name "DisplayVersion" | Out-Null        #
 }                                                                                                                                           # Crea DisplayVersion
