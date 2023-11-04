a2ab64634368c5db9ecfb1d794f957ca6fbc34c8
Zarckash <zarckash@gmail.com>
2023-11-04 15:17:40 +0100

    Fix Latencia button

--- Functions/Functions.ps1
+++ Functions/Functions.ps1
@@ -910,7 +910,7 @@ function UpdateGPUDrivers {
 }
 
 function LatencyTweaks {
-    Write-UserOutput "Aplicando tweaks de latencia"
+    Write-UserOutput "Aplicando ajustes para reducir input lag"
     powercfg -setacvalueindex scheme_current 54533251-82be-4824-96c1-47b60b740d00 4d2b0152-7d5c-498b-88e2-34345392a2c5 5000
     $ClassGuid = (Get-PnpDevice -Class Display).ClassGuid
     $RegPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Class\$ClassGuid"
