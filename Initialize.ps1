371631c46217d635ba9f6c1dfc4b5c5f1cc493b0
Zarckash <zarckash@gmail.com>
2024-01-04 07:56:53 +0100

    Update Initialize.ps1

--- Initialize.ps1
+++ Initialize.ps1
@@ -7,7 +7,7 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 
 New-Item "$env:temp\ZKTool\Files" -ItemType Directory -Force | Out-Null
 
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/Zarckash/ZKTool/raw/main/Resources.zip/ZKTool.zip","$env:temp\ZKTool\Files\ZKTool.zip")
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/Zarckash/ZKTool/raw/main/Resources/ZKTool.zip","$env:temp\ZKTool\Files\ZKTool.zip")
 Expand-Archive -Path "$env:temp\ZKTool\Files\ZKTool.zip" -DestinationPath "$env:ProgramFiles\ZKTool" -Force
 
 Start-Process "$env:ProgramFiles\ZKTool\Setup.exe"
