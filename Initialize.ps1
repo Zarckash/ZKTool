3642d9a1ff93d5933230194f869ebaef1c0a73c9
Zarckash <zarckash@gmail.com>
2024-01-04 07:55:19 +0100

    Create Initialize.ps1

---
+++ Initialize.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'SilentlyContinue'
+$ProgressPreference = 'SilentlyContinue'
+$WarningPreference = 'SilentlyContinue'
+$ConfirmPreference = 'None'
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Type DWord -Value 0
+
+New-Item "$env:temp\ZKTool\Files" -ItemType Directory -Force | Out-Null
+
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/Zarckash/ZKTool/raw/main/Resources.zip/ZKTool.zip","$env:temp\ZKTool\Files\ZKTool.zip")
+Expand-Archive -Path "$env:temp\ZKTool\Files\ZKTool.zip" -DestinationPath "$env:ProgramFiles\ZKTool" -Force
+
+Start-Process "$env:ProgramFiles\ZKTool\Setup.exe"
