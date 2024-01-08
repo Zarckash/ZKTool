4a5de39a78fdfaf710d19cfd3aac287caad31727
Zarckash <zarckash@gmail.com>
2024-01-08 13:58:15 +0100

    Update Initialize.ps1

--- Initialize.ps1
+++ Initialize.ps1
@@ -1,19 +1,18 @@
-﻿Start-Process PowerShell -WindowStyle Hidden {
-    Set-ExecutionPolicy Bypass
+﻿$ProgressPreference = 'SilentlyContinue'
+$ErrorActionPreference = 'SilentlyContinue'
 
-    $ProgressPreference = 'SilentlyContinue'
+Set-ExecutionPolicy Bypass
 
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Type DWord -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableLUA" -Type DWord -Value 0
 
-    Add-MpPreference -ExclusionPath "$env:temp\ZKTool\"
-    Add-MpPreference -ExclusionPath "$env:ProgramFiles\ZKTool"
+Add-MpPreference -ExclusionPath "$env:temp\ZKTool\"
+Add-MpPreference -ExclusionPath "$env:ProgramFiles\ZKTool"
 
-    New-Item "$env:temp\ZKTool\Files\" -ItemType Directory -Force | Out-Null
+New-Item "$env:temp\ZKTool\Files\" -ItemType Directory -Force | Out-Null
 
-    (New-Object System.Net.WebClient).DownloadFile("https://github.com/Zarckash/ZKTool/raw/main/Resources/ZKTool.zip","$env:temp\ZKTool\Files\ZKTool.zip")
-    Expand-Archive -Path "$env:temp\ZKTool\Files\ZKTool.zip" -DestinationPath "$env:temp\ZKTool\Files\Temp" -Force
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/Zarckash/ZKTool/raw/main/Resources/ZKTool.zip", "$env:temp\ZKTool\Files\ZKTool.zip")
+Expand-Archive -Path "$env:temp\ZKTool\Files\ZKTool.zip" -DestinationPath "$env:temp\ZKTool\Files\Temp" -Force
 
-    Start-Process "$env:temp\ZKTool\Files\Temp\Setup.exe" -ArgumentList "-Install"
-}
+Start-Process "$env:temp\ZKTool\Files\Temp\Setup.exe" -ArgumentList "-Install"
 
 exit
