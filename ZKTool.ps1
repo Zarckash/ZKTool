228cf2b4bab058c0f84fdbea943cc3ac530de4e7
Zarckash <zarckash@gmail.com>
2024-01-08 12:53:50 +0100

    Setup fixed

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -5,7 +5,7 @@ $ProgressPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $ConfirmPreference = 'None'
 
-$App.Version = "4.1.9"
+$App.Version = "4.2.0"
 
 if (!((Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZKTool" -Name "DisplayVersion") -eq $App.Version)) {
     if (!(Test-Path "$env:ProgramFiles\ZKTool\Setup.exe")) {
