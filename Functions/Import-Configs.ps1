ce51ee6ccb090ee38bef1d8d5ccc3649b704061e
Zarckash <zarckash@gmail.com>
2024-06-30 09:05:17 +0200

    Add Plutonium zombies config

--- Functions/Import-Configs.ps1
+++ Functions/Import-Configs.ps1
@@ -44,3 +44,9 @@ function RivaTuner {
     Move-Item -Path ($App.FilesPath + "RivaTuner\Config") -Destination 'C:\Program Files (x86)\RivaTuner Statistics Server\ProfileTemplates' -Force
     Write-UserOutput ("Configuracion de " + $App.ConfigsList.Config16.Name + " aplicada")
 }
+
+function Plutonium {
+    $App.Download.DownloadFile(($App.GitHubFilesPathPath + ".zip/Plutonium.zip"), ($App.FilesPath + "Plutonium.zip"))
+    Expand-Archive -Path ($App.FilesPath + "Plutonium.zip") -DestinationPath ("$env:localappdata\Plutonium") -Force
+    Write-UserOutput ("Configuracion de " + $App.ConfigsList.Config17.Name + " aplicada")
+}
