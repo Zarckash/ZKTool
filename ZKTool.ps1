bba090ed2eb870d1bbec5c759d4869b5764d5f06
Zarckash <zarckash@gmail.com>
2024-10-14 09:46:21 +0200

    Change font installation method

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -5,7 +5,7 @@ $ProgressPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $ConfirmPreference = 'None'
 
-$App.Version = "4.3.3"
+$App.Version = "4.3.4"
 
 if (!((Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZKTool" -Name "DisplayVersion") -eq $App.Version)) {
     Start-Process Powershell -WindowStyle Hidden{
