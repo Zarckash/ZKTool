0099b5cf243a862de997e02d2e18acc4770b4121
Zarckash <zarckash@gmail.com>
2025-05-08 02:30:19 +0200

    Remove pc restart check after every script run

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -283,26 +283,6 @@ $PwShellGUI.AddScript({
             }
             & Reset-Buttons
 
-            # Checking Restart
-            if ($App.RequireRestart) {
-                Write-UserOutput "Reinicio necesario"
-                $MessageBox = [System.Windows.Forms.MessageBox]::Show("El equipo requiere reiniciarse para aplicar los cambios`r`nReiniciar equipo ahora?", "Reiniciar equipo", [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Information)
-                if ($MessageBox -ne [System.Windows.Forms.DialogResult]::No) {
-                    Write-UserOutput "Reiniciando pc en 5 segundos"
-                    Start-Sleep 1
-                    4..1 | ForEach-Object {
-                        Update-GUI OutputBox Text "Reiniciando pc en $_ segundos..."
-                        Start-Sleep 1
-                    }
-                    Start-Process Powershell -WindowStyle Hidden {
-                        Start-Sleep 1
-                        Get-Process "ZKTool" | Stop-Process
-                        Remove-Item -Path "$env:temp\ZKTool\Files" -Recurse -Force
-                        Restart-Computer
-                    }
-                }
-            }
-
             Update-GUI StartScript Content "INICIAR SCRIPT"
             Update-GUI StartScript Background $App.HoverColor
             Update-GUI OutputBox Text "Script finalizado"
