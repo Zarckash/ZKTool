24708521e19a3cd3139077932d3d2a0f607df8b5
Zarckash <zarckash@gmail.com>
2025-08-04 20:55:27 +0200

    Remove colors close to white

--- Functions/Set-AccentColor.ps1
+++ Functions/Set-AccentColor.ps1
@@ -6,28 +6,40 @@
 
     $App.AccentColor = "#"
     $Color = $Color - 1
+    $DefaultAppColor = "#33CCCC"
 
     $GetWindowsAccentColor = (Get-ItemPropertyValue -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent" -Name "AccentPalette") | Select-Object -Skip ($Color*4) -First 3
 
-    $GetWindowsAccentColor | ForEach-Object {
-        if ($_ -lt 10) {
-            $App.AccentColor += ("0$_").ToUpper()
-        }
-        else {
-            $App.AccentColor += ([convert]::Tostring($_, 16)).ToUpper()
-        }
-    }
 
-    if ($App.AccentColor -eq "#4CC2FF") {
-        $App.AccentColor = "#ACA5F3" # Default app color
-    }
-
-    if (($App.AccentColor -eq "#000000") -or ($App.AccentColor -eq "#FFFFFF")) {
+    if (($GetWindowsAccentColor[0] -ge 225) -and ($GetWindowsAccentColor[1] -ge 225) -and ($GetWindowsAccentColor[2] -ge 225)) {
         if ($Color -lt 5) {
             Set-AccentColor ($Color + 3)
         }
         else {
-            $App.AccentColor = "#ACA5F3"
+            $App.AccentColor = $DefaultAppColor
+        }
+    }
+    else {
+        $GetWindowsAccentColor | ForEach-Object {
+            if ($_ -lt 10) {
+                $App.AccentColor += ("0$_").ToUpper()
+            }
+            else {
+                $App.AccentColor += ([convert]::Tostring($_, 16)).ToUpper()
+            }
+        }
+    }
+
+    if ($App.AccentColor -eq "#4CC2FF") {
+        $App.AccentColor = $DefaultAppColor
+    }
+
+    if ($App.AccentColor -eq "#000000") {
+        if ($Color -lt 5) {
+            Set-AccentColor ($Color + 3)
+        }
+        else {
+            $App.AccentColor = $DefaultAppColor
         }
     }
 
