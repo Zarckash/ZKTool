622884e5fed92193a59f4000afac648bf4862562
Zarckash <zarckash@gmail.com>
2025-08-09 14:05:03 +0200

    Update Import-Configs.ps1

--- Functions/Import-Configs.ps1
+++ Functions/Import-Configs.ps1
@@ -130,7 +130,7 @@ function RivaTuner {
     Move-Item -Path ($App.FilesPath + "RivaTuner\HotkeyHandler.cfg") -Destination 'C:\Program Files (x86)\RivaTuner Statistics Server\Plugins\Client' -Force
     Move-Item -Path ($App.FilesPath + "RivaTuner\Overlays\*") -Destination 'C:\Program Files (x86)\RivaTuner Statistics Server\Plugins\Client\Overlays' -Force
     Get-ChildItem $Path | ForEach-Object {
-        (Get-Content $_.FullName) -replace ('PositionX=.*','PositionX=6') -replace ('PositionY=.*','PositionY=1') -replace ('SyncLimiter=.*','SyncLimiter=3') -replace ('PassiveWait=.*','PassiveWait=0') -replace ('Face=.*','Face=GeForce') -replace ('Weight=.*','Weight=700') | Set-Content $_.FullName
+        (Get-Content $_.FullName) -replace ('PositionX=.*','PositionX=6') -replace ('PositionY=.*','PositionY=1') -replace ('SyncLimiter=.*','SyncLimiter=3') -replace ('PassiveWait=.*','PassiveWait=0') -replace ('Face=.*','Face=GeForce') -replace ('Weight=.*','Weight=700') -replace ('ZoomRatio=.*','ZoomRatio=1') | Set-Content $_.FullName
     }
 
     if ($MSIABRunning) {
