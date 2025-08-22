140c9a803cda5281dd8d2dd00676ce5ee30f6e38
Zarckash <zarckash@gmail.com>
2025-08-22 18:17:38 +0200

    Update ZKTool.ps1

--- ZKTool.ps1
+++ ZKTool.ps1
@@ -12,7 +12,7 @@ $ProgressPreference = 'SilentlyContinue'
 $WarningPreference = 'SilentlyContinue'
 $ConfirmPreference = 'None'
 
-$App.Version = "4.5.9"
+$App.Version = "4.6"
 
 if (!((Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\ZKTool" -Name "DisplayVersion") -eq $App.Version)) {
     Start-Process Powershell -WindowStyle Hidden {
